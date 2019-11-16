1db835fbc24e826530dc4900b14bb99ace1a49fe
superterminal <trendemil@gmail.com>
2019-11-16 22:27:25 +0200

    added data from sample json api

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import Post from './Post/Post';
+import postService  from '../services/post-service';
 
 import './Posts.css';
 
-function Posts() {
-    return (
-        <div className="Posts">
-            <Post imageUrl="logo192.png" imageAlt="origami" author="some author">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta ex accusamus vero eligendi aut molestiae ratione ipsam sit rem autem. Cumque itaque amet eius eos! Nihil nisi delectus culpa facilis!</Post>
-        </div>
-    )
+class Posts extends React.Component {
+    state = {
+        posts: null
+    }
+
+    componentDidMount() {
+        postService.load()
+            .then(posts => {
+                this.setState({ posts });
+            });
+    }
+
+    render() {
+        const { posts } = this.state;
+        return posts ? 
+            <div className="Posts">
+                {posts.map(post => (
+                    <Post key={post.id} imageUrl="/logo192.png" imageAlt="post" author={post.userId}>{post.body}</Post>
+                ))}
+            </div>
+         : <div>Loading...</div>
+    }
 }
 
 export default Posts;
